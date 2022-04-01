63e0bd2690f93c951a3385a6a761d995b243fa4a
unknown <panu.raikkonen@metropolia.fi>
2022-04-01 14:09:42 +0300

    asd

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,11 +1,24 @@
+import {Button} from '@mui/material';
+import {useState} from 'react';
 import LoginForm from '../components/LoginForm';
 import RegisterForm from '../components/RegisterForm';
 
 const Login = () => {
+  const [toggle, setToggle] = useState(true);
   return (
     <>
-      <LoginForm />
-      <RegisterForm />
+      {toggle ? <LoginForm /> : <RegisterForm />}
+
+      <Button
+        fullWidth
+        color="primary"
+        variant="contained"
+        onClick={() => {
+          setToggle(!toggle);
+        }}
+      >
+        {toggle ? 'Register' : 'Login'}
+      </Button>
     </>
   );
 };
